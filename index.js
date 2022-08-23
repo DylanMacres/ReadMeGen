26ce808393ae6d494ea910b7f1dfda1981519415
DylanMacres <Dylan.Macres@du.edu>
2022-08-23 17:24:48 -0600

    psuedo code plus npm install

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const inquirer = require('inquirer')
+
+//questions to ask
+
+
+//answers data
+
+//catch error
+
+
+//function to make the Readme file 
+
+//fuction to intialize the app 
+
+//call the init 
