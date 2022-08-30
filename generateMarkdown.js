312a32c264482e1c1f2d715f3658e03027b1660a
DylanMacres <Dylan.Macres@du.edu>
2022-08-30 17:52:39 -0600

    fixed the readme gen and kept in old code to show where i was going

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,10 +1,29 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(data) {
+  const licenseArray = data.license[0];
+  let licenseString = "";
+  if(licenseArray === "Apache"){
+    console.log("yay!!")
+    licenseString = `![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`
+  }else if(licenseArray === 'Mozilla'){
+    licenseString = `![Lincese: Mozilla](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`
+  }else if(licenseArray === 'MIT'){
+    licenseString = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  }else if(licenseArray === "Boost"){
+    licenseString = `![License: Boost](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)`
+  }else{
+    return licenseString;
+  }
+   
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) {
+//   let link = "";
+
+// }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -13,6 +32,39 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+    
+  ## Description
+    ${data.description}
+
+  ## Table Of Contents:
+
+  1. [Description](#description)
+  2. [Installation](#Installation)
+  3. [Usage](#Usage)
+  4. [Contributions](#credits)
+  5. [GitHub](#github)
+  6. [Email](#email)
+  7. [Licenses](#license)
+
+
+
+  ## Installation 
+    ${data.Installation}
+
+  ## Usage
+   ${data.Usage}
+
+  ## Contributions 
+    ${data.credits}
+
+  ## Github
+    ${data.github}
+
+  ## E-Mail
+    ${data.email}
+
+  ## License
+    ${renderLicenseBadge(data)}
 
 `;
 }
--- index.js
+++ index.js
@@ -1,12 +1,115 @@
+// function init (){
+
 const fs = require('fs')
 const inquirer = require('inquirer')
+const generateMarkdown = require("./generateMarkdown")
+const path = require('path')
+const fileName = "README.md"
 
+//add the common licenses information perhaps in an array
+//was going to use the array in the generate markdown js file but got stuck so i switched it up 
+
+
+
+
+//  const licenseArray = [
+//     {
+//         name:'Apache',
+//         url:"https://www.apache.org/licenses/LICENSE-2.0",
+//         img:"https://img.shields.io/badge/License-Apache_2.0-blue.svg",
+//     },
+//     {
+//         name:'Mozilla',
+//         url:"https://www.mozilla.org/en-US/MPL/2.0/",
+//         img:"https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg",
+//     },
+//     {
+//         name:'MIT',
+//         url:"https://opensource.org/licenses/MIT",
+//         img:"https://img.shields.io/badge/License-MIT-yellow.svg",
+//     },
+
+//     {
+//         name:'Boost',
+//         url:"https://www.boost.org/LICENSE_1_0.txt",
+//         img:"https://img.shields.io/badge/License-Boost_1.0-lightblue.svg",
+//     }
+//  ]
 //questions to ask
+let questions = ([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?'
+    },
+    {
+        type: 'input ',
+        name: 'Description',
+        message: "Please describe what your project is!",
+    },
+    {
+        type: 'input',
+        name: 'Installation',
+        message: "Provide instuctions for a user to install"
+    },
+    {
+        type:"input",
+        name:'Usage',
+        message:'Please specify the usage information'
+    },
+    {
+        type:"input",
+        name:'credits',
+        message:'Please specify the contributions'
+    },
+    {
+        type:"input",
+        name:'github',
+        message:'Please include your Github username'
+    },
+    {
+        type:"input",
+        name:'email',
+        message:'Please include your email address'
+    },
+    {
+        type: 'list',
+        name:"license",
+        message: "What license did you use for this repository?",
+        choices: [
+            'Apache',
+            'Mozilla',
+            'MIT',
+            'Boost'
 
 
-//answers data
+        ]
+    }
+])
+// .then((results) => {
+//     console.log(results)
 
+//     fs.writeFile('README.md', )
+// })
+function writeFile(fileName, data){
+    const markdownjs = generateMarkdown(data);
+    fs.writeFile(fileName, markdownjs, function (err){
+        if (err) {
+            console.log('this does not work')
+        }
+    })
+}
+    
 //catch error
+//answers data
+function init() {
+    inquirer.prompt(questions)
+.then (function (data) {
+    writeFile(fileName, data)
+})
+}
+
+init()
 
 
 //function to make the Readme file 
